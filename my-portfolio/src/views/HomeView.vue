bd8ec2aeaea92d766d5e8e237239199cbf40a655
Sean He <hesixiang8@163.com>
2025-11-27 15:01:25 +0800

    second save

---
+++ my-portfolio/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Hero from '../components/Hero.vue'
+import About from '../components/About.vue'
+</script>
+
+<template>
+  <Header />
+  <main class="main">
+    <Hero />
+    <About />
+  </main>
+</template>
