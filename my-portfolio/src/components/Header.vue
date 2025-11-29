d856f52811ffa122a3462f56ffbe65790bb774a1
Sean He <hesixiang8@163.com>
2025-11-29 10:36:04 +0800

    nav first

--- my-portfolio/src/components/Header.vue
+++ my-portfolio/src/components/Header.vue
@@ -5,30 +5,20 @@
       <a href="index.html" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">DevFolio</h1>
+        <h1 class="sitename">何思响个人网站</h1>
       </a>
 
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="#hero" class="active">Home<br></a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+          <li><a href="#hero" class="active">首页<br></a></li>
+          <li><a href="#about">简介</a></li>
+          <li><a href="#services">教育经历</a></li>
+          <li><a href="#portfolio">个人技能</a></li>
+          <li class="dropdown"><a href="#"><span>个人相册</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
-              <li><a href="#">Dropdown 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                <ul>
-                  <li><a href="#">Deep Dropdown 1</a></li>
-                  <li><a href="#">Deep Dropdown 2</a></li>
-                  <li><a href="#">Deep Dropdown 3</a></li>
-                  <li><a href="#">Deep Dropdown 4</a></li>
-                  <li><a href="#">Deep Dropdown 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Dropdown 2</a></li>
-              <li><a href="#">Dropdown 3</a></li>
-              <li><a href="#">Dropdown 4</a></li>
+              <li><a href="#">自拍</a></li>
+              <li><a href="#">美食</a></li>
+              <li><a href="#">风景</a></li>
             </ul>
           </li>
           <li><a href="#contact">Contact</a></li>
--- my-portfolio/src/components/Hero.vue
+++ my-portfolio/src/components/Hero.vue
@@ -2,11 +2,11 @@
   <!-- Hero Section -->
   <section id="hero" class="hero section dark-background">
 
-    <img src="/assets/img/hero-img.jpg" alt="" data-aos="fade-in">
+    <img src="/assets/img/西装淡蓝.png" alt="" data-aos="fade-in">
 
     <div class="container d-flex flex-column align-items-center justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
-      <h2>I am Morgan Freeman</h2>
-      <p><span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+      <h2>我是何思响</h2>
+      <p><span class="typed" data-typed-items="湖南师范大学, 大三, 教育技术学, cs极客"></span></p>
     </div>
 
   </section><!-- /Hero Section -->
